dfd6165b6a939de747579ac474791a8cd8f8670d
Tadas Giniotis <mail@copper.lt>
2020-08-22 14:56:20 +0300

    to make it work

--- build-copr.sh
+++ build-copr.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+export LC_ALL=C.UTF-8
 
 set -euo pipefail
 
@@ -13,35 +14,12 @@ readonly baserelease=$(grep '%global baserelease' $spec_file | awk '{print $NF}'
 
 readonly version=$major_version.$base_sublevel.$stable_update-$baserelease
 
-readonly archs=(\
-    "k8"\
-    "k8sse3"\
-    "k10"\
-    "bobcat"\
-    "bulldozer"\
-    "piledriver"\
-    "steamroller"\
-    "excavator"\
-    "jaguar"\
-    "zen"\
-    "core2"\
-    "nehalem"\
-    "westmere"\
-    "sandybridge"\
-    "ivybridge"\
-    "haswell"\
-    "broadwell"\
-    "skylake"\
-    )
-
 # default to git branch
 readonly branch=${1:-$(git rev-parse --abbrev-ref HEAD)}
 readonly release=${branch#f}
 
-for arch in "${archs[@]}"; do
-    chroot=fedora-$release-x86_64
-    project=kernel-gcc-$arch
-    filename=kernel-$version.$arch.fc$release.src.rpm
+chroot=fedora-$release-x86_64
+project=linux-kernel-ck
+filename=kernel-$version.fc$release.src.rpm
 
-    copr-cli build --chroot $chroot --nowait $project $filename
-done
+copr-cli build --chroot $chroot --nowait $project $filename
